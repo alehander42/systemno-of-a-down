0d872cdd2855553306d21d4a899201a29a4382a7
alehander42 <alehander42@gmail.com>
2013-11-18 17:48:58 +0200

    C is love \0

--- wc_l.c
+++ wc_l.c
@@ -1,19 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void fail(char* message) {
   printf("wc: %s\n", message);
   exit(1);
 }
 
-int count_stdin_chars() {
+int count_stdin_lines() {
   int count = 0;
-  while(getchar() != EOF) {
-    count++;
+  char c;
+  while((c = getchar()) != EOF) {
+    if(c == '\n') {
+      count++;
+    }
   }
   return count;
 }
 
-int count_file_chars(char* filename) {
+int count_file_lines(char* filename) {
   FILE* file = fopen(filename, "r");
   if(!file) {
     char message[256];
@@ -21,9 +24,11 @@ int count_file_chars(char* filename) {
     fail(message);
   }
 
-  int count = 0;
-  while(fgetc(file) != EOF) {
-    count++;
+  int count = 0; char c;
+  while((c = fgetc(file)) != EOF) {
+    if(c == '\n') {
+      count++;
+    }
   }
   return count;
 }
@@ -32,18 +37,18 @@ int main(int argc, char* argv[]) {
   int count = 0;
 
   if(argc == 1) {
-    count = count_stdin_chars();
+    count = count_stdin_lines();
     printf("%d\n", count);
   }
   else if(argc == 2) {
-    count = count_file_chars(argv[1]);
+    count = count_file_lines(argv[1]);
     printf("%d %s\n", count, argv[1]);
   }
   else {
     int i;
     int j;
     for(i = 1; i < argc;i ++) {
-      j = count_file_chars(argv[i]);
+      j = count_file_lines(argv[i]);
       printf("%d %s\n", j, argv[i]);
       count += j;
     }
@@ -51,4 +56,3 @@ int main(int argc, char* argv[]) {
   }
   return 0;
 }
-
